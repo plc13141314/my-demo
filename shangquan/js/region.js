c34d88da59fb574b6215a24579ca0ced34755102
guoyonghao <guoyonghao@yy.com>
2015-03-06 17:37:57 +0800

    commit

---
+++ shangquan/js/region.js
@@ -0,0 +1,99 @@
+{
+        "data":{
+        "region":
+        [
+            {"id": "440103", "name": "\u8354\u6e7e\u533a", "children": [
+                {"id": "30", "name": "\u4e0a\u4e0b\u4e5d\u5546\u4e1a\u533a"},
+                {"id": "31", "name": "\u4e2d\u5c71\u4e03\u516b\u8def"},
+                {"id": "32", "name": "\u897f\u6751\/\u897f\u573a"},
+                {"id": "33", "name": "\u82b3\u6751"},
+                {"id": "34", "name": "\u6c99\u9762"},
+                {"id": "35", "name": "\u8354\u6e7e\u533a\u5176\u4ed6"}
+            ]},
+            {"id": "440104", "name": "\u8d8a\u79c0\u533a", "children": [
+                {"id": "11", "name": "\u5317\u4eac\u8def\u5546\u4e1a\u533a"},
+                {"id": "12", "name": "\u73af\u5e02\u4e1c\u8def\u6cbf\u7ebf"},
+                {"id": "13", "name": "\u4e2d\u5c71\u4e8c\u4e09\u8def\/\u4e1c\u5c71\u53e3"},
+                {"id": "15", "name": "\u6cbf\u6c5f\u8def\u6cbf\u7ebf\/\u4e8c\u6c99\u5c9b"},
+                {"id": "17", "name": "\u706b\u8f66\u7ad9\/\u4eba\u6c11\u5317\u8def"},
+                {"id": "14", "name": "\u4e94\u7f8a\u65b0\u57ce"},
+                {"id": "16", "name": "\u4e2d\u5c71\u516d\u8def"},
+                {"id": "18", "name": "\u4e1c\u98ce\u4e2d\u8def\/\u8d8a\u79c0\u516c\u56ed"},
+                {"id": "19", "name": "\u9e93\u6e56\u516c\u56ed\u5468\u8fb9"},
+                {"id": "20", "name": "\u8d8a\u79c0\u533a\u5176\u4ed6"}
+            ]},
+            {"id": "440105", "name": "\u6d77\u73e0\u533a", "children": [
+                {"id": "21", "name": "\u6c5f\u5357\u897f\u5546\u4e1a\u533a"},
+                {"id": "24", "name": "\u6c5f\u5357\u5927\u9053\u6cbf\u7ebf"},
+                {"id": "26", "name": "\u6ee8\u6c5f\u8def\u6cbf\u7ebf"},
+                {"id": "25", "name": "\u5de5\u4e1a\u5927\u9053\u6cbf\u7ebf"},
+                {"id": "22", "name": "\u5ba2\u6751\/\u8d64\u5c97"},
+                {"id": "23", "name": "\u7436\u6d32"},
+                {"id": "27", "name": "\u65b0\u6e2f\u897f\u8def\u6cbf\u7ebf"},
+                {"id": "28", "name": "\u4e1c\u6653\u5357\u8def\u6cbf\u7ebf"},
+                {"id": "29", "name": "\u6d77\u73e0\u533a\u5176\u4ed6"}
+            ]},
+            {"id": "440106", "name": "\u5929\u6cb3\u533a", "children": [
+                {"id": "1", "name": "\u5929\u6cb3\u57ce\/\u4f53\u80b2\u4e2d\u5fc3"},
+                {"id": "2", "name": "\u5929\u6cb3\u5317\/\u5e7f\u5dde\u4e1c\u7ad9"},
+                {"id": "3", "name": "\u73e0\u6c5f\u65b0\u57ce\/\u8dd1\u9a6c\u573a"},
+                {"id": "4", "name": "\u77f3\u724c\/\u9f99\u53e3"},
+                {"id": "6", "name": "\u8f66\u9642\/\u4e1c\u5703"},
+                {"id": "7", "name": "\u71d5\u5cad\/\u4e94\u5c71"},
+                {"id": "8", "name": "\u9f99\u6d1e\/\u5c91\u6751"},
+                {"id": "9", "name": "\u6c99\u6cb3\/\u5929\u5e73\u67b6"},
+                {"id": "10", "name": "\u5929\u6cb3\u533a\u5176\u4ed6"},
+                {"id": "5", "name": "\u5929\u6cb3\u516c\u56ed\/\u68e0\u4e0b"}
+            ]},
+            {"id": "440111", "name": "\u767d\u4e91\u533a", "children": [
+                {"id": "42", "name": "\u673a\u573a\u8def\u6cbf\u7ebf"},
+                {"id": "36", "name": "\u5e7f\u56ed\u65b0\u6751"},
+                {"id": "37", "name": "\u4e09\u5143\u91cc"},
+                {"id": "38", "name": "\u65b0\u5e02"},
+                {"id": "39", "name": "\u540c\u5fb7\u56f4"},
+                {"id": "40", "name": "\u540c\u548c\/\u4eac\u6eaa"},
+                {"id": "41", "name": "\u7f57\u51b2\u56f4\/\u91d1\u6c99\u6d32"},
+                {"id": "43", "name": "\u767d\u4e91\u5927\u9053\u6cbf\u7ebf"},
+                {"id": "44", "name": "\u767d\u4e91\u533a\u5176\u4ed6"}
+            ]},
+            {"id": "440112", "name": "\u9ec4\u57d4\u533a", "children": [
+                {"id": "271", "name": "\u9ec4\u57d4"}
+            ]},
+            {"id": "440113", "name": "\u756a\u79ba\u533a", "children": [
+                {"id": "48", "name": "\u5927\u5b66\u57ce"},
+                {"id": "45", "name": "\u5e02\u6865"},
+                {"id": "46", "name": "\u6d1b\u6d66"},
+                {"id": "47", "name": "\u5927\u77f3"},
+                {"id": "49", "name": "\u949f\u6751"},
+                {"id": "50", "name": "\u5357\u6751"},
+                {"id": "51", "name": "\u77f3\u7881"},
+                {"id": "52", "name": "\u756a\u79ba\u533a\u5176\u4ed6"}
+            ]},
+            {"id": "440114", "name": "\u82b1\u90fd\u533a", "children": [
+                {"id": "272", "name": "\u82b1\u90fd"}
+            ]},
+            {"id": "440115", "name": "\u5357\u6c99\u533a", "children": []},
+            {"id": "440116", "name": "\u841d\u5c97\u533a", "children": []},
+            {"id": "440183", "name": "\u589e\u57ce\u5e02", "children": []},
+            {"id": "440184", "name": "\u4ece\u5316\u5e02", "children": []}
+        ],
+            "hot_buss_region":
+        [
+            {"id": "1", "name": "\u5929\u6cb3\u57ce\/\u4f53\u80b2\u4e2d\u5fc3"},
+            {"id": "2", "name": "\u5929\u6cb3\u5317\/\u5e7f\u5dde\u4e1c\u7ad9"},
+            {"id": "3", "name": "\u73e0\u6c5f\u65b0\u57ce\/\u8dd1\u9a6c\u573a"},
+            {"id": "4", "name": "\u77f3\u724c\/\u9f99\u53e3"},
+            {"id": "11", "name": "\u5317\u4eac\u8def\u5546\u4e1a\u533a"},
+            {"id": "12", "name": "\u73af\u5e02\u4e1c\u8def\u6cbf\u7ebf"},
+            {"id": "21", "name": "\u6c5f\u5357\u897f\u5546\u4e1a\u533a"},
+            {"id": "13", "name": "\u4e2d\u5c71\u4e8c\u4e09\u8def\/\u4e1c\u5c71\u53e3"},
+            {"id": "17", "name": "\u706b\u8f66\u7ad9\/\u4eba\u6c11\u5317\u8def"},
+            {"id": "31", "name": "\u4e2d\u5c71\u4e03\u516b\u8def"},
+            {"id": "24", "name": "\u6c5f\u5357\u5927\u9053\u6cbf\u7ebf"},
+            {"id": "26", "name": "\u6ee8\u6c5f\u8def\u6cbf\u7ebf"},
+            {"id": "25", "name": "\u5de5\u4e1a\u5927\u9053\u6cbf\u7ebf"},
+            {"id": "48", "name": "\u5927\u5b66\u57ce"},
+            {"id": "42", "name": "\u673a\u573a\u8def\u6cbf\u7ebf"}
+        ]
+    }
+}
